21721a1e997b1d4e87e2e8e58cfb5319c8e7203a
Anar <anarochkakot@gmail.com>
2023-03-31 16:00:25 +0400

    add getter for date

--- src/main/java/ru/pchelicam/cbrf/services/CbRateService.java
+++ src/main/java/ru/pchelicam/cbrf/services/CbRateService.java
@@ -1,18 +1,25 @@
 package ru.pchelicam.cbrf.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Component
 public class CbRateService {
 
+    @Lazy
     @Autowired
     private DailyInfoSoap dailyInfoSoap;
 
     public BigDecimal getCbRate() {
         return dailyInfoSoap.MainInfoXML().getRegData().getCbRate().getCbRateValue();
+    }
+
+    public LocalDate getCbRateUpdatedDate() {
+        return dailyInfoSoap.MainInfoXML().getRegData().getCbRate().getUpdatedDate();
     }
 
 }
